cbce67d9a498dd6b60c8eb4d4c6d2e8ecebb5cd5
Tharindu Thilanka <tharindupro89@gmail.com>
2024-01-29 00:12:38 -0800

    Bug Fix  - change type to optional

--- app/src/main/java/com/omdb/movie/domain/movie/MediaType.kt
+++ app/src/main/java/com/omdb/movie/domain/movie/MediaType.kt
@@ -4,8 +4,8 @@ enum class MediaType(val type: String) {
     MOVIE("movie"), SERIES("series"), EPISODE("episode");
 
     companion object {
-        fun fromType(type: String): MediaType {
-            return entries.find { it.type == type } ?: throw Error("Couldn't find Media Type")
+        fun fromType(type: String): MediaType? {
+            return entries.find { it.type == type }
         }
     }
 }
--- app/src/main/java/com/omdb/movie/domain/movie/MovieData.kt
+++ app/src/main/java/com/omdb/movie/domain/movie/MovieData.kt
@@ -3,7 +3,7 @@ package com.omdb.movie.domain.movie
 data class MovieData(
     val id: String,
     val title: String,
-    val type: MediaType,
+    val type: MediaType? = null,
     val year: String,
     val image: String
 )
